709e226331c1efdb2f1a9c861ec83093ef7ed61a
Selfex <artem.selfex@gmail.com>
2024-12-15 14:05:04 +0700

    feat: dropdown list component implemented

---
+++ src/shared/ui/dropDownList/ui/index.tsx
@@ -0,0 +1,27 @@
+import { FC, JSX } from 'react';
+import clsx from 'clsx';
+
+import './index.scss';
+
+interface DropDownListProps {
+  content?: JSX.Element | JSX.Element[];
+  className?: string;
+  isOpen?: boolean;
+}
+
+export const DropDownList: FC<DropDownListProps> = ({
+  content,
+  className,
+  isOpen,
+}) => {
+  return (
+    <div
+      className={clsx('dropdown', className, {
+        dropdown__opened: isOpen,
+        dropdown__closed: !isOpen,
+      })}
+    >
+      <div className="dropdown__content">{content}</div>
+    </div>
+  );
+};
