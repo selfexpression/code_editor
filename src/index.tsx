4cf417dd9eaeabf91ebff595965dc7fb70b1f0e3
Selfex <artem.selfex@gmail.com>
2024-12-16 20:34:42 +0700

    refactor: css variables added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
-import './global.css';
+import './globals.scss';
 
 const container = document.querySelector('#root') as HTMLElement;
 const root = createRoot(container);
